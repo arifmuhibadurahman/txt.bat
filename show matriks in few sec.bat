8dfed3c9cdc1854872f73ce2d3df143613da2324
01.pm <106901786+arifmuhibadurahman@users.noreply.github.com>
2024-01-16 13:24:02 +0700

    Add files via upload

---
+++ show matriks in few sec.bat
@@ -0,0 +1,5 @@
+@echo off
+color 02 //Kode warna CMD
+:start
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+goto start
